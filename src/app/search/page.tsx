7a7a4f96a3a98e5c0e4b536c1afc7dc5a6bf8843
haiduongfc2001 <haiduongfc2001@gmail.com>
2024-04-17 17:28:05 +0700

    dhd-011: build a ui search results component to display hotel search results

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 import * as React from "react";
-import { Box, List, ListItem, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { useSearchParams } from "next/navigation";
 import SearchBar from "@/components/search/search-hotel";
 import dayjs from "dayjs";
+import SearchResult from "@/components/search/search-result";
 
 export default function Search() {
   const searchParams = useSearchParams();
@@ -19,28 +20,49 @@ export default function Search() {
 
   return (
     <>
-      <Box
-        sx={{
-          color: "#1a202c",
-          my: 4,
-          mx: 2,
-          position: "relative",
-          fontSize: "14px",
-          fontWeight: "normal",
-          lineHeight: "17px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <SearchBar
-          location={location}
-          checkInDate={dayjs(checkInDate)}
-          checkOutDate={dayjs(checkOutDate)}
-          numberOfPeople={numberOfPeople}
-          numberOfRooms={numberOfRooms}
-        />
-      </Box>
+      <React.Suspense fallback={null}>
+        <Box
+          sx={{
+            color: "#1a202c",
+            my: 4,
+            mx: 2,
+            position: "relative",
+            fontSize: "14px",
+            fontWeight: "normal",
+            lineHeight: "17px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <SearchBar
+            location={location}
+            checkInDate={dayjs(checkInDate)}
+            checkOutDate={dayjs(checkOutDate)}
+            numberOfPeople={numberOfPeople}
+            numberOfRooms={numberOfRooms}
+          />
+        </Box>
+        <Box sx={{ display: "flex" }}>
+          {/* Sidebar for filters */}
+          <Box
+            sx={{
+              flex: "0 0 25%",
+              borderRight: "1px solid #ccc",
+              padding: "0 20px",
+            }}
+          >
+            <Typography variant="h6" gutterBottom>
+              Filters
+            </Typography>
+            {/* Add filter components here */}
+            {/* Example: <FilterComponent /> */}
+          </Box>
+
+          {/* Main section for hotel display */}
+          <SearchResult location={location} />
+        </Box>
+      </React.Suspense>
     </>
   );
 }
---
+++ src/components/search/search-result.tsx
@@ -0,0 +1,292 @@
+"use client";
+import { FC } from "react";
+import * as React from "react";
+import { Box, CardMedia, Grid, Stack, Typography } from "@mui/material";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import formatCurrency from "@/utils/format-currency";
+
+interface SearchResultProps {
+  location: string;
+}
+
+const SearchResult: FC<SearchResultProps> = ({ location }) => {
+  const hotels = [
+    {
+      hotel_id: 38527,
+      hotel_name: "Khách sạn La Vague",
+      hotel_address: "Số 8, Trần Phú, Thành Phố Nha Trang, Khánh Hòa, Việt Nam",
+      hotel_avatar:
+        "https://gcs.tripi.vn/hms_prod/photo/thumb/1616771153173ME/outside.png",
+      min_room_price: 900000,
+      original_room_price: 1000000,
+    },
+    {
+      hotel_id: 48801,
+      hotel_name: "Khách sạn Grand Gosia Nha Trang",
+      hotel_address:
+        "10-12 Trần Phú, Vĩnh Nguyên, Thành Phố Nha Trang, Khánh Hòa, Việt Nam",
+      hotel_avatar:
+        "https://storage.googleapis.com/hms_prod/photo/thumb/1617092342927GY/144944532.png",
+      min_room_price: 900000,
+      original_room_price: 1000000,
+    },
+    {
+      hotel_id: 38483,
+      hotel_name: "Khách sạn Sea Pearl Nha Trang",
+      hotel_address: "98a, Trần Phú, Thành Phố Nha Trang, Khánh Hòa, Việt Nam",
+      hotel_avatar:
+        "https://gcs.tripi.vn/hms_prod/photo/thumb/1617174369612dF/172059893.jpg",
+      min_room_price: 900000,
+      original_room_price: 1000000,
+    },
+    {
+      hotel_id: 48130,
+      hotel_name: "The Empyrean Nha Trang",
+      hotel_address:
+        "2 , Nguyễn Thị Minh Khai, Thành Phố Nha Trang, Khánh Hòa, Việt Nam",
+      hotel_avatar:
+        "https://gcs.tripi.vn/hms_prod/photo/thumb/1616819671281ZO/40510723.png",
+      min_room_price: 900000,
+      original_room_price: 1000000,
+    },
+    {
+      hotel_id: 38437,
+      hotel_name: "Khách sạn Ibis Styles Nha Trang",
+      hotel_address: "86, Hùng Vương, Thành Phố Nha Trang, Khánh Hòa, Việt Nam",
+      hotel_avatar:
+        "https://gcs.tripi.vn/hms_prod/photo/img/467043orJ/z4260616666973_ae897d5bc64a75a0a960e005004c5497.jpg",
+      min_room_price: 900000,
+      original_room_price: 1000000,
+    },
+    {
+      hotel_id: 38380,
+      hotel_name: "Khách sạn Boton Blue & Spa Nha Trang",
+      hotel_address:
+        "Phạm Văn Đồng, Phường Vĩnh Hòa, Phạm Văn Đồng, Thành Phố Nha Trang, Khánh Hòa, Việt Nam",
+      hotel_avatar:
+        "https://gcs.tripi.vn/hms_prod/photo/thumb/2685xI/ecea6c41b2d197bd06460bee7ab542b0.jpg",
+      min_room_price: 900000,
+      original_room_price: 1000000,
+    },
+    {
+      hotel_id: 38406,
+      hotel_name: "TUI BLUE - Khách sạn TUI BLUE Nha Trang",
+      hotel_address:
+        "18, Trần Hưng Đạo, Thành Phố Nha Trang, Khánh Hòa, Việt Nam",
+      hotel_avatar:
+        "https://gcs.tripi.vn/hms_prod/photo/img/470238wvF/dji_0056.jpg",
+      min_room_price: 900000,
+      original_room_price: 1000000,
+    },
+    {
+      hotel_id: 38664,
+      hotel_name: "Khách Sạn Mường Thanh Luxury Nha Trang",
+      hotel_address:
+        "60, Trần Phú, Lộc Thọ, Thành Phố Nha Trang, Khánh Hòa, Việt Nam",
+      hotel_avatar:
+        "https://gcs.tripi.vn/hms_prod/photo/thumb/1620472922630mr/293542180_result.jpg",
+      min_room_price: 900000,
+      original_room_price: 1000000,
+    },
+    {
+      hotel_id: 9723,
+      hotel_name: "Khách Sạn Mường Thanh Grand Nha Trang",
+      hotel_address:
+        "6 , Dương Hiến Quyền, Thành Phố Nha Trang, Khánh Hòa, Việt Nam",
+      hotel_avatar:
+        "https://gcs.tripi.vn/hms_prod/photo/thumb/1616860211117eh/283800904.png",
+      min_room_price: 900000,
+      original_room_price: 1000000,
+    },
+    {
+      hotel_id: 45768,
+      hotel_name: "Khách sạn Merperle Beach Nha Trang",
+      hotel_address: "88a, Trần Phú, Thành Phố Nha Trang, Khánh Hòa, Việt Nam",
+      hotel_avatar:
+        "https://googleapis.tripi.vn/download/storage/v1/b/tourcdn/o/photos%2FDETKKSYJY7_%2Ftmp%2Fplaytemp7486223076244920531%2FmultipartBody4363323379706674084asTemporaryFile?generation=1592549175846288&alt=media",
+      min_room_price: 900000,
+      original_room_price: 1000000,
+    },
+    {
+      hotel_id: 38470,
+      hotel_name: "Khách Sạn Majestic Premium Nha Trang",
+      hotel_address:
+        "96b5/4 , Trần Phú, Thành Phố Nha Trang, Khánh Hòa, Việt Nam",
+      hotel_avatar:
+        "https://gcs.tripi.vn/hms_prod/photo/thumb/1617199586684xK/152261603.jpg",
+      min_room_price: 900000,
+      original_room_price: 1000000,
+    },
+    {
+      hotel_id: 512,
+      hotel_name: "Khách sạn Sheraton Nha Trang",
+      hotel_address:
+        "26-28, Trần Phú, Thành Phố Nha Trang, Khánh Hòa, Việt Nam",
+      hotel_avatar:
+        "https://www.googleapis.com/download/storage/v1/b/hotel_image/o/logo%2F4%2F210187.jpg?generation=1563771731644355&alt=media",
+      min_room_price: 900000,
+      original_room_price: 1000000,
+    },
+    {
+      hotel_id: 553,
+      hotel_name: "Khách Sạn Novotel Nha Trang",
+      hotel_address: "50, Trần Phú, Thành Phố Nha Trang, Khánh Hòa, Việt Nam",
+      hotel_avatar:
+        "https://googleapis.tripi.vn/download/storage/v1/b/tourcdn/o/photos%2FXMS2N0BE6X_%2Ftmp%2Fplaytemp4401775770632333474%2FmultipartBody6774088289307231415asTemporaryFile?generation=1586178743406910&alt=media",
+      min_room_price: 900000,
+      original_room_price: 1000000,
+    },
+    {
+      hotel_id: 8496,
+      hotel_name: "Khách sạn StarCity Nha Trang",
+      hotel_address:
+        "72-74 , Trần Phú, Lộc Thọ, Thành Phố Nha Trang, Khánh Hòa, Việt Nam",
+      hotel_avatar:
+        "https://gcs.tripi.vn/hms_prod/photo/thumb/1616818433346JL/out.png",
+      min_room_price: 900000,
+      original_room_price: 1000000,
+    },
+    {
+      hotel_id: 38396,
+      hotel_name: "Vinpearl  Beachfront Nha Trang",
+      hotel_address:
+        "78-80 Trần Phú, Phường Lộc Thọ, Trần Phú, Lộc Thọ, Thành Phố Nha Trang, Khánh Hòa, Việt Nam",
+      hotel_avatar:
+        "https://gcs.tripi.vn/hms_prod/photo/thumb/1616841710477jp/dai-dien.png",
+      min_room_price: 900000,
+      original_room_price: 1000000,
+    },
+    {
+      hotel_id: 2048,
+      hotel_name: "Champa Island Nha Trang - Resort Hotel & Spa   ",
+      hotel_address: "304 , 2/4, Thành Phố Nha Trang, Khánh Hòa, Việt Nam",
+      hotel_avatar:
+        "https://gcs.tripi.vn/hms_prod/photo/img/469422dbJ/z4420199007786_a80da51a029391ded0f8199ec2230a6a.jpg",
+      min_room_price: 900000,
+      original_room_price: 1000000,
+    },
+  ];
+
+  return (
+    <Box sx={{ flex: "1", p: "0 20px" }}>
+      <Box
+        width="100%"
+        height="100px"
+        m={2}
+        p={2}
+        bgcolor="background.paper"
+        borderRadius="8px"
+        display="flex"
+        justifyContent="center"
+        flexDirection="column"
+      >
+        <Typography variant="h5">{location}</Typography>
+        <Typography component="span">
+          {`Có ${hotels?.length ?? 0} khách sạn ở ${location}`}
+        </Typography>
+      </Box>
+
+      <Grid container spacing={3}>
+        {/* Display hotels as cards */}
+        {hotels.map((hotel) => (
+          <Grid item xs={12} key={hotel.hotel_id}>
+            <Box
+              sx={{
+                width: "100%",
+                mx: 2,
+                p: 2,
+                borderRadius: 1,
+                overflow: "hidden",
+                boxShadow: "0px 5px 5px rgba(0, 0, 0, 0.1)",
+                bgcolor: "background.paper",
+                display: "flex",
+                "&:hover .MuiTypography-subtitle1": {
+                  color: "primary.main",
+                  transition: "all .2s",
+                },
+              }}
+            >
+              <Grid container spacing={2}>
+                <Grid item sm={12} md={4}>
+                  <CardMedia
+                    component="img"
+                    src={
+                      hotel?.hotel_avatar ||
+                      "https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg"
+                    }
+                    alt={hotel.hotel_name}
+                    sx={{
+                      height: "150px",
+                      objectFit: "cover",
+                      borderRadius: 1,
+                    }}
+                  />
+                </Grid>
+                <Grid item sm={12} md={5}>
+                  <Box>
+                    <Typography variant="h5" gutterBottom>
+                      {hotel.hotel_name}
+                    </Typography>
+                    <Stack direction="row">
+                      <LocationOnIcon />
+                      <Typography
+                        variant="body2"
+                        sx={{
+                          overflow: "hidden",
+                          display: "-webkit-box",
+                          webkitLineClamp: 2,
+                          webkitBoxOrient: "vertical",
+                          textOverflow: "ellipsis",
+                        }}
+                      >
+                        {hotel.hotel_address}
+                      </Typography>
+                    </Stack>
+                  </Box>
+                </Grid>
+                <Grid item sm={12} md={3}>
+                  <Box
+                    p={2}
+                    sx={{
+                      width: "100%",
+                      height: "100%",
+                      display: "flex",
+                      alignItems: "flex-end",
+                      flexDirection: "column",
+                      justifyContent: "flex-end",
+                    }}
+                  >
+                    <Typography
+                      variant="body2"
+                      sx={{
+                        color: "#718096",
+                        textDecoration: "line-through",
+                        cursor: "pointer",
+                      }}
+                      component="span"
+                    >
+                      {formatCurrency(hotel.original_room_price)}
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      sx={{
+                        color: "rgb(229, 62, 62)",
+                        fontWeight: 600,
+                        cursor: "pointer",
+                      }}
+                      component="span"
+                    >
+                      {formatCurrency(hotel.min_room_price)}
+                    </Typography>
+                  </Box>
+                </Grid>
+              </Grid>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+export default SearchResult;
